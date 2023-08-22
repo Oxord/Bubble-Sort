9e0ee6ecd750e94fc0526db3c936dc0a269ffbd7
Oxord <142917803+Oxord@users.noreply.github.com>
2023-08-22 18:26:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите последовательность чисел через пробел: ");
+            string NumbersFromConsole = Console.ReadLine();
+            List<string> NumbersStr = NumbersFromConsole.Split(' ').ToList();
+            List<int> numbers = NumbersStr.Select(int.Parse).ToList();
+            int temp;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
